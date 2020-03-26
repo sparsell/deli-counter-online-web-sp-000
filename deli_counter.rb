5653fc59b6634b1d04f696d19c1e1a653a9c6dd8
Sonja Parsell <hello@sparsell.com>
2020-03-26 13:04:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
+katz_deli = ["Logan", "Avi", "Spencer", "Harry", "Charles", "Amber", "Scarlett"]
+
+def line(katz_deli)
+     if katz_deli.length == 0
+       puts "The line is currently empty."
+     else
+      katz_deli.each.with_index do |customer, index|
+        katz = katz_deli.push ("#{index + 1}. #{customer}")
+
+      end
+      puts "The line is currently:" + katz_deli_place
+    end
+end
 # Write your code here.
