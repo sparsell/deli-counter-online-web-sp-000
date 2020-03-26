a574fd2e9349bb474e060de77d34d113f601bd07
Sonja Parsell <hello@sparsell.com>
2020-03-26 13:05:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
        puts "The line is currently empty."
      else
       katz_deli.each_with_index do |customer, index|
-        katz = katz_deli.push ("#{index + 1}. #{customer}")
+      katz_deli.push ("#{index + 1}. #{customer}")
 
       end
       puts "The line is currently:" + katz_deli_place
