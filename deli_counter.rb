35796a8cae57fcf8c680ebe36bd7c33767078a53
Sonja Parsell <hello@sparsell.com>
2020-03-26 13:58:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,7 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts katz_deli.first
+    puts "Currently serving" + "#{katz_deli.first}.""
     katz_deli.shift
   end
 end
