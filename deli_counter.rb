ada063957927fd1a50d2bc4de5c7604fab8e8c23
Sonja Parsell <hello@sparsell.com>
2020-03-26 14:16:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,8 +19,8 @@ def take_a_number(katz_deli, string)
       puts "Welcome, #{string}. You are number 1 in line."
     else
       katz_deli.length != 0
-      katz_deli.push "#{string}"
-      puts "Welcome, #{string}. You are number #{katz_deli.length +1} in line."
+      katz_deli.push ("#{string}")
+      puts "Welcome, #{string}. You are number #{katz_deli.length + 1} in line."
 end
 end
 
