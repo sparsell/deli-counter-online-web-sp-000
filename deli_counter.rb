e000e79af4406b6e0d87057a8953535698ff959e
Sonja Parsell <hello@sparsell.com>
2020-03-26 13:07:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
      if katz_deli.length == 0
        puts "The line is currently empty."
      else
-       katz_customers = []
+       katz_deli_customers = []
       katz_deli.each_with_index do |customer, index|
       katz_deli_customers.push ("#{index + 1}. #{customer}")
       end
