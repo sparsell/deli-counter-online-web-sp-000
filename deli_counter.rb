1a4de9355d53241f1d2ad12beca139d2619678d6
Sonja Parsell <hello@sparsell.com>
2020-03-26 13:53:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,5 @@
-katz_deli = ["Logan", "Avi", "Spencer", "Harry", "Charles", "Amber", "Scarlett"]
+katz_deli = []
+#katz_deli = ["Logan", "Avi", "Spencer", "Harry", "Charles", "Amber", "Scarlett"]
 
 def line(katz_deli)
      if katz_deli.length == 0
